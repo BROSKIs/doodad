53a10a1a52c7587502667d6914e57664adb79102
KJ <kjmiclea@gmail.com>
2026-03-10 12:11:21 -0700

    Renamed admin to admin-users. Added comments, added admin to nav.

--- app.js
+++ app.js
@@ -13,23 +13,27 @@ const contacts = [];
 
 const users = []
 
-//IN NAVBAR
+// home page (nav)
 app.get("/", (req,res)=>{
     res.render("home");
 });
 
+// login page (nav)
 app.get("/login", (req, res) => {
     res.render("login");
 });
+
+// sign up page (nav)
 app.get("/sign-up", (req, res)=>{
     res.render("sign-up");
 });
 
+// contact us (nav)
 app.get("/contact", (req, res)=>{
     res.render("contact");
 });
 
-//NOT IN NAVBAR
+// post-sign-up page
 app.post("/new-account", (req,res)=>{
     const user = {
         fname: req.body.fname,
@@ -42,7 +46,7 @@ app.post("/new-account", (req,res)=>{
     res.render("new-account", { user });
 });
 
-
+// post-contact page
 app.post("/contact-submitted", (req, res)=>{
     const contact = {
         name: req.body.name,
@@ -55,16 +59,19 @@ app.post("/contact-submitted", (req, res)=>{
     res.render("conf-contact");
 })
 
+// post-login page
 app.post("/logged-in", (req, res) => {
     res.render("home");
 });
 
+// access admin page for contacts
 app.get("/admin-contact", (req, res)=>{
     res.send(contacts);
 });
 
+// access admin page for users
 app.get("/admin-users", (req, res)=>{
-    res.render('admin', { users });
+    res.render('admin-users', { users });
 });
 
 app.listen(PORT, ()=>{
