8631cc6abedf865af03da85b150ce047355c326b
KJ <kjmiclea@gmail.com>
2026-03-18 12:32:02 -0700

    Base framework for validation

---
+++ validate.js
@@ -0,0 +1,13 @@
+export function validateContact(data) {
+    console.log("Starting contact validation...");
+    console.log(data);
+
+    const errors = [];
+}
+
+export function validateItem(data) {
+    console.log("Starting item listing validation...");
+    console.log(data);
+
+    const errors = [];
+}
