f6e8a528d0ab280d68efb2c1f10f30543be36862
KJ <kjmiclea@gmail.com>
2026-03-18 23:09:49 -0700

    Did the following: Added inline errors to create_item.ejs Added inline validation for the client in create_item.ejs

--- public/js/item-val.js
+++ public/js/item-val.js
@@ -5,6 +5,34 @@ function validate() {
     clearErrors();
     let name = document.getElementById("name").value.trim();
     let email = document.getElementById("email").value.trim();
+    let price = document.getElementById("price").value;
+    let desc = document.getElementById("desc").value.trim();
+
+    // if name is empty
+    if(!name){
+        isValid = false;
+        document.getElementById("name-err").style.display = "inline-block";
+    }
+    
+    // if email is empty or email is not valid
+    if(!valid(email) || !email){
+        isValid = false;
+        document.getElementById("email-err").style.display = "inline-block";
+    }
+
+    // if price is below zero
+    if (price < 0) {
+        isValid = false;
+        document.getElementById("price-err").style.display = "inline-block";
+    }
+
+    // if item description is empty
+    if(!desc) {
+        isValid = false;
+        document.getElementById("desc-err").style.display = "inline-block";
+    }
+
+    return isValid;
 }
 
 // function to clear errors
